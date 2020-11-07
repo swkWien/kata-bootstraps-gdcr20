5aecb7a7c6610a4c87b713f386bc636621dffb15
Alexander Kosik <alexander.kosik@aareal-bank.com>
2020-11-07 11:16:28 +0100

    add init_empty_field test

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -8,3 +8,9 @@ def test_underpopulation():
     assert not field[5][5]
 
 
+def test_init_empty_field():
+    field = create_field()
+    for row in field:
+        for column in row:
+            assert column is False
+
