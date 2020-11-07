c7b691187b004f691e0417e4e579876f54cd348f
Christoph Buchner <bilderbuchi@phononoia.at>
2020-11-07 11:31:21 +0100

    Create tests and game rule

--- python/pytest/app/game_of_life.py
+++ python/pytest/app/game_of_life.py
@@ -1,7 +1,22 @@
 def tick(field):
     """Progress a field one tick forward and return it."""
-    field[5][5] = False
+    for row_idx, row in enumerate(field):
+        for col_idx, col in enumerate(row):
+            count = count_living_neighbors(field, (row_idx, col_idx))
+            new_state = game_rule(neighbour_count=count, state=col)
+            # TODO: continue here
+            field[5][5] = False
     return field
+
+
+def count_living_neighbors(field, cell: tuple):
+    count = 0
+    return count
+
+
+def game_rule(neighbour_count, state):
+    """Depending on the neighbour count and current state, return the new state."""
+    return state and neighbour_count == 2 or neighbour_count == 3
 
 
 def create_field(*args):
--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,5 +1,6 @@
-#
-from app.game_of_life import tick, create_field
+import pytest
+
+from app.game_of_life import tick, create_field, game_rule
 
 
 def test_underpopulation():
@@ -14,3 +15,33 @@ def test_init_empty_field():
         for column in row:
             assert column is False
 
+
+def test_overcrowding():
+    field = create_field((4, 5),
+                         (5, 4), (5, 5), (5, 6),
+                         (6, 5))
+    field = tick(field)
+    assert not field[5][5]
+
+@pytest.mark.skip()
+def test_stay_alive():
+    field = create_field((5, 4), (5, 5), (5, 6),
+                         (6, 5))
+    field = tick(field)
+    assert field[5][5]
+
+
+def test_overcrowding_rule():
+    assert not game_rule(neighbour_count=4, state=True)
+
+
+def test_stay_alive_rule():
+    assert game_rule(neighbour_count=2, state=True)
+    assert game_rule(neighbour_count=3, state=True)
+
+
+def test_lonely_rule():
+    assert not game_rule(neighbour_count=1, state=True)
+
+def test_born_rule():
+    assert game_rule(neighbour_count=3, state=False)
