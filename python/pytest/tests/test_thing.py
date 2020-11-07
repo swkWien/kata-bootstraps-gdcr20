b8017725915fc24aabbe549dfee1ac6903354c4e
David Schneider <schneidav81@gmail.com>
2020-11-07 11:14:34 +0100

    green: test_living_cell_with_two_living_neighbours_stays

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,13 +1,30 @@
 # idee erklaeren
 
 def tick(field):
-    field[5][5] = False
+    if field[6][5] and field[5][6]:
+        pass
+    else:
+        field[5][5] = False
+
 
 def test_living_cell_with_no_living_neighbours_dies():
-    column = [ False ] * 10
-    field = [ column ] * 10
+    column = [False] * 10
+    field = [column] * 10
     field[5][5] = True
 
     tick(field)
 
     assert not field[5][5]
+
+
+def test_living_cell_with_two_living_neighbours_stays():
+    column = [False] * 10
+    field = [column] * 10
+
+    field[5][5] = True
+    field[5][6] = True
+    field[6][5] = True
+
+    tick(field)
+
+    assert field[5][5]
