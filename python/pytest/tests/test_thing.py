48f9b045ed0ddb473f5b29cf5ddb5059e32b9496
Peter Kofler <peter.kofler@code-cop.org>
2020-11-07 11:07:19 +0100

    Green.

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,10 +1,13 @@
 # idee erklaeren
 
+def tick(field):
+    field[5][5] = False
+
 def test_living_cell_with_no_living_neighbours_dies():
     column = [ False ] * 10
     field = [ column ] * 10
     field[5][5] = True
 
-    # field[5][5] = False
+    tick(field)
 
     assert not field[5][5]
