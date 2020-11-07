0534b0af0a6b855a9a2dc5ae42be6b7f2203558d
David Schneider <schneidav81@gmail.com>
2020-11-07 10:08:57 +0100

    create test_underpopulation

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,2 +1,12 @@
-def test_correct_greeting():
-    assert True
+#
+
+
+class TestRule1:
+    def test_underpopulation(self):
+        # create empty field
+        field = [[0] * 10]*10
+        field[5][5] = 1
+
+        field[5][5] = 0
+
+        assert field[5][5] == 0
