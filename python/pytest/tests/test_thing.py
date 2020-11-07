074d09623c165687adab19889bdac1cee5424c4f
Peter Kofler <peter.kofler@code-cop.org>
2020-11-07 11:05:51 +0100

    Red: test_living_cell_with_no_living_neighbours_dies

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,4 +1,10 @@
+# idee erklaeren
 
+def test_living_cell_with_no_living_neighbours_dies():
+    column = [ False ] * 10
+    field = [ column ] * 10
+    field[5][5] = True
 
-def test_correct_greeting():
-    assert "a" == "n"
+    # field[5][5] = False
+
+    assert not field[5][5]
