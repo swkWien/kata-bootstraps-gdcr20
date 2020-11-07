9453bd47e2a8a9af92fb59f31a57d18b5cddc6fa
David Schneider <schneidav81@gmail.com>
2020-11-07 12:31:13 +0100

    create count_living_neighbours function

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,10 +1,29 @@
 import pytest
 
 
+def count_living_neighbours(field, x, y):
+    cnt = 0
+
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if i == j:
+                continue
+            if x < 0:
+                continue
+            if y < 0:
+                continue
+            if x >= len(field):
+                continue
+            if x >= len(field[0]):
+                continue
+            cnt += int(field[i][j])
+    return cnt
+
+
 def tick(field):
     for y, row in enumerate(field[:-1]):
         for x, cell in enumerate(row[:-1]):
-            if field[x + 1][y] and field[x][y + 1]:
+            if count_living_neighbours(field, x, y) == 2:
                 pass
             else:
                 field[x][y] = False
@@ -30,9 +49,23 @@ def test_living_cell_with_no_living_neighbours_dies(x, y, alive):
     assert not field[x][y]
 
 
-def test_living_cell_with_two_living_neighbours_stays():
-    field = create_field((5, 5), (5, 6), (6, 5))
+@pytest.mark.parametrize("x,y,alive", [
+    (5, 5, [(5, 6), (6, 5)]),
+    (5, 5, [(4, 5), (5, 4)]),
+])
+def test_living_cell_with_two_living_neighbours_stays(x, y, alive):
+    field = create_field(*[(x, y)] + alive)
 
     tick(field)
 
-    assert field[5][5]
+    assert field[x][y]
+
+
+@pytest.mark.parametrize("x,y,alive", [
+    (5, 5, [(5, 6), (6, 5)]),
+    (5, 5, [(4, 5), (5, 4)]),
+])
+def test_count_living_cell_with_two_living_neighbours_stays(x, y, alive):
+    field = create_field(*[(x, y)] + alive)
+
+    assert count_living_neighbours(field, x, y) == 2
