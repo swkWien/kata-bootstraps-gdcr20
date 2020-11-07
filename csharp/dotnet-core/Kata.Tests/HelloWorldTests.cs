3f69c2cce281662fe729c9881789510f474b9d01
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-11-07 10:10:49 +0100

    add initial test

--- csharp/dotnet-core/Kata.Tests/HelloWorldTests.cs
+++ csharp/dotnet-core/Kata.Tests/HelloWorldTests.cs
@@ -6,14 +6,32 @@ namespace Kata.Tests
     public class HelloWorldTests
     {
         [Fact]
-        public void SayHelloWorld_ShouldReturnHelloWorld()
+        public void FindNeighbors_NoNeighbors_Zero()
         {
             // Arrange
-            var sut = new HelloWorld();
+            var world = new World();
+            var cell = world.GetCell();
             // Act
-            var actual = sut.SayHelloWorld();
+            var actual = world.FindLivingNeighborsOf(cell);
             // Asseert
-            actual.Should().Be("Oh oh...");
+            actual.Should().Be(0);
+        }
+    }
+
+    class Cell
+    {
+    }
+
+    class World
+    {
+        internal int FindLivingNeighborsOf(Cell cell)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal Cell GetCell()
+        {
+            return new Cell();
         }
     }
 }
