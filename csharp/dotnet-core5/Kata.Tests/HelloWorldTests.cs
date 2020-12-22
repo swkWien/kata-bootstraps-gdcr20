0a09867d7967752d4ccd5f2b9ba9c4a4583c11cd
Patrick Drechsler <patrick@pdrechsler.de>
2020-12-22 11:36:44 +0100

    test with no neighbours

--- csharp/dotnet-core5/Kata.Tests/HelloWorldTests.cs
+++ csharp/dotnet-core5/Kata.Tests/HelloWorldTests.cs
@@ -8,8 +8,8 @@ namespace Kata.Tests
         [Fact]
         public void Cell_survives_when_it_has_2_living_neighbours()
         {
-            Cell cell = new Cell();
-            int numberOfLivingNeighbours = 2;
+            var cell = new Cell();
+            var numberOfLivingNeighbours = 2;
             var result = CellSurvives(cell, numberOfLivingNeighbours);
             result.Should().BeTrue();
         }
@@ -17,15 +17,24 @@ namespace Kata.Tests
         [Fact]
         public void Cell_dies_when_it_has_1_living_neighbour()
         {
-            Cell cell = new Cell();
-            int numberOfLivingNeighbours = 1;
+            var cell = new Cell();
+            var numberOfLivingNeighbours = 1;
             var result = CellSurvives(cell, numberOfLivingNeighbours);
-            result.Should().BeFalse("Should die with less than 2 living neighbours");
+            result.Should().BeFalse("Should die with less than 2 living neighbours (1)");
+        }
+
+        [Fact]
+        public void Cell_dies_when_no_living_neighbours_are_present()
+        {
+            var cell = new Cell();
+            var numberOfLivingNeighbours = 0;
+            var result = CellSurvives(cell, numberOfLivingNeighbours);
+            result.Should().BeFalse("Should die with less than 2 living neighbours (0)");
         }
 
         private bool CellSurvives(Cell cell, in int numberOfLivingNeighbours)
         {
-            return true;
+            return numberOfLivingNeighbours != 1;
         }
     }
 
