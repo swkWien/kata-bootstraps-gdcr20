b3aa3ad99e7776756db53e1dce5fddee33e4634f
r.quidet <r.quidet@sportradar.com>
2020-12-22 11:32:40 +0100

    first test.

---
+++ php/tests/GridTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use swkberlin\gol\Grid;
+
+class GridTest extends TestCase
+{
+    public function testSomething()
+    {
+        $width = 10;
+        $height = 10;
+        $grid = new Grid($width, $height);
+        $expected = $this->getBoard();
+
+        $board = $grid->getBoard();
+
+        $this->assertEquals($expected, $board);
+    }
+
+    private function getBoard()
+    {
+        $board = [];
+        for ($height = 0; $height = 0 < 10; $height++) {
+            for ($width = 0; $width < 10; $width++) {
+                $board[$height][$width] = NULL;
+            }
+        }
+
+        return $board;
+    }
+}
