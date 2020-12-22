9c8345eb7066d3b3c16f814d8144a23815b90636
christopher <christopher.zelezny@mailbox.org>
2020-12-22 11:22:08 +0100

    first test is Cell alive

---
+++ java/junit5/src/test/java/CellTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CellTest {
+
+    @Test
+    void it_should_not_fail() {
+        assertTrue(true);
+    }
+
+    @Test
+    void cell_should_check_if_it_alive(){
+        Cell cell = new Cell(0,0,true);
+        assertEquals(true, cell.getIsAlive());
+    }
+
+
+
+}
