a6d632d7bef3d10b34b32b4b38a4fbed94fbad93
j.bozali <j.bozali@sportradar.com>
2020-12-22 11:42:39 +0100

    Fix 1st Test

--- php/src/Grid.php
+++ php/src/Grid.php
@@ -6,12 +6,25 @@ namespace swkberlin\gol;
 
 class Grid
 {
-	private $width;
-	private $height;
+	private $width = 0;
+	private $height = 0;
 
 	public function __construct($width, $height)
 	{
+		$this->width = $width;
+		$this->height = $height;
 
+	}
+
+	public function getBoard()
+	{
+		$board = [];
+		for ($this->height; $this->height < 10; $this->height++) {
+			for ($this->width; $this->width < 10; $this->width++) {
+				$board[$this->height][$this->width] = NULL;
+			}
+		}
+		return $board;
 	}
 
 }
--- php/tests/GridTest.php
+++ php/tests/GridTest.php
@@ -12,22 +12,21 @@ class GridTest extends TestCase
         $width = 10;
         $height = 10;
         $grid = new Grid($width, $height);
-        $expected = $this->getBoard();
+        $expected = $this->getBoard($height, $width);
 
         $board = $grid->getBoard();
 
         $this->assertEquals($expected, $board);
     }
 
-    private function getBoard()
+    private function getBoard($height, $width)
     {
         $board = [];
-        for ($height = 0; $height = 0 < 10; $height++) {
+        for ($height = 0; $height < 10; $height++) {
             for ($width = 0; $width < 10; $width++) {
                 $board[$height][$width] = NULL;
             }
         }
-
         return $board;
     }
 }
