60ebfa386bb5da183cf891bdbb864f362e2670d2
Patrick Drechsler <patrick@pdrechsler.de>
2020-12-23 02:49:20 +0100

    move implementation to class

--- csharp/dotnet-core5/Kata.Tests/HelloWorldTests.cs
+++ csharp/dotnet-core5/Kata.Tests/HelloWorldTests.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -28,40 +25,8 @@ namespace Kata.Tests
         [InlineData(true, 6, false)]
         [InlineData(true, 7, false)]
         [InlineData(true, 8, false)]
-        public void Cell_rules(bool isAlive, int numberOfLivingNeighbours, bool expected)
-        {
-            var result = DoesCellSurvive(isAlive, numberOfLivingNeighbours);
-            result.Should().Be(expected);
-        }
-
-        private bool DoesCellSurvive(bool isAlive, int numberOfLivingNeighbours)
-        {
-            var tuples = new List<(bool Alive, int Num, bool Expected)>
-            {
-                new (false, 0, false),
-                new (false, 1, false),
-                new (false, 2, false),
-                new (false, 3, true),
-                new (false, 4, false),
-                new (false, 5, false),
-                new (false, 6, false),
-                new (false, 7, false),
-                new (false, 8, false),
-
-                new (true, 0, false),
-                new (true, 1, false),
-                new (true, 2, true),
-                new (true, 3, true),
-                new (true, 4, false),
-                new (true, 5, false),
-                new (true, 6, false),
-                new (true, 7, false),
-                new (true, 8, false),
-            };
-
-            return tuples
-                .First(x => x.Num == numberOfLivingNeighbours && x.Alive == isAlive)
-                .Expected;
-        }
+        public void Cell_rules(bool isAlive, int numberOfLivingNeighbours, bool expected) =>
+            HelloWorld.DoesCellSurvive(isAlive, numberOfLivingNeighbours)
+                .Should().Be(expected);
     }
 }
--- csharp/dotnet-core5/Kata/HelloWorld.cs
+++ csharp/dotnet-core5/Kata/HelloWorld.cs
@@ -1,10 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Kata
 {
     public class HelloWorld
     {
-        public string SayHelloWorld()
+        public static bool DoesCellSurvive(bool isAlive, int numberOfLivingNeighbours)
         {
-            return "Hello World!";
+            var tuples = new List<(bool Alive, int Num, bool Expected)>
+            {
+                new (false, 0, false),
+                new (false, 1, false),
+                new (false, 2, false),
+                new (false, 3, true),
+                new (false, 4, false),
+                new (false, 5, false),
+                new (false, 6, false),
+                new (false, 7, false),
+                new (false, 8, false),
+
+                new (true, 0, false),
+                new (true, 1, false),
+                new (true, 2, true),
+                new (true, 3, true),
+                new (true, 4, false),
+                new (true, 5, false),
+                new (true, 6, false),
+                new (true, 7, false),
+                new (true, 8, false),
+            };
+
+            return tuples
+                .First(x => x.Num == numberOfLivingNeighbours && x.Alive == isAlive)
+                .Expected;
         }
     }
 }
