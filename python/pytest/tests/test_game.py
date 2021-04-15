cffa64251b6013bad8596b1e3ed12b1a64941ade
Claus <claus.aichinger@mailbox.org>
2021-04-15 11:11:39 +0200

    DEV: add first test

---
+++ python/pytest/tests/test_game.py
@@ -0,0 +1,28 @@
+"""
+Components and Concepts
+=======================
+
+Grid
+Cell are elements of the grid
+Cell has a coordinate (it is a node in the grid)
+Cell has a state (dead or alive)
+
+Neighborhood
+A cell needs to know about it's neighborhood
+
+Game
+Game "holds" the cells
+Game can evolve cells (from current state to next state)
+
+Approach
+========
+
+Build the system outside in, starting with (overly) simplified
+inputs.
+We aim for OO.
+"""
+
+
+def test_game_without_arguments_does_not_hold_any_cells():
+    game = Game()
+    assert game.cells == {}
