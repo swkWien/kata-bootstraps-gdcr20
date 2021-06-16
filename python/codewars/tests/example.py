6a8f324f28c2d22f9358e3635031d5d9b3cd10ab
nadaa <nada.saif7@gmail.com>
2021-06-17 00:53:07 +0200

    test

---
+++ python/codewars/tests/example.py
@@ -0,0 +1,9 @@
+import codewars_test as test
+
+
+@test.describe('Example Tests')
+def example_tests():
+    @test.it('Example Test Case')
+    def example_test_case():
+        test.assert_equals(42, 42, 'Optional Message on Failure')
+        test.assert_equals(43, 42, 'Failure')
