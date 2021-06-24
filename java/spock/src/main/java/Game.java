0600187e832a249f79166cc16147248acfefd3c2
Adam Zielinski <github@maosmurf.com>
2021-06-24 12:07:38 +0200

    A game of #n x #m has #fieldCount fields

--- java/spock/src/main/java/Game.java
+++ java/spock/src/main/java/Game.java
@@ -6,10 +6,10 @@ public class Game {
     private final List<Field> fields;
 
     public Game(int n, int m) {
-        this.fields = seed();
+        this.fields = seed(n, m);
     }
 
-    private ArrayList<Field> seed() {
+    private ArrayList<Field> seed(int n, int m) {
         ArrayList<Field> fields = new ArrayList<>();
         fields.add(new Field() {});
         return fields;
--- java/spock/src/test/groovy/GameSpec.groovy
+++ java/spock/src/test/groovy/GameSpec.groovy
@@ -2,15 +2,21 @@ import spock.lang.Specification
 
 class GameSpec extends Specification {
 
-    def "There exists a game with fields"() {
+    def "A game of #n x #m has #fieldCount fields"() {
         given:
-        def game = new Game(1, 1)
+        def game = new Game(n, m)
 
         when:
         def fields = game.fields
 
         then:
-        fields.size()  == 1
+        fields.size()  == fieldCount
+
+        where:
+        n | m || fieldCount
+        1 | 1 || 1
+        2 | 2 || 4
+
     }
 
 }
