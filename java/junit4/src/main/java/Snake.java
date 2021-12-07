736a0d3daef642512afe1da632033fb2d7744e7d
Trygve Nerland <t.nerland@sportradar.com>
2021-12-07 11:32:15 +0100

    Add snake direction test

---
+++ java/junit4/src/main/java/Snake.java
@@ -0,0 +1,14 @@
+public class Snake {
+  private int direction = 0;
+
+  public Snake() {
+  }
+
+  public int getDirection() {
+    return direction;
+  }
+
+  public void setDirection(int direction) {
+
+  }
+}
--- java/junit4/src/test/java/ThingTest.java
+++ java/junit4/src/test/java/ThingTest.java
@@ -7,15 +7,23 @@ import org.junit.Test;
  */
 public class ThingTest {
 
-    @Test
-    public void it_should_call_for_action() {
-        Thing thing = new Thing();
-        String value = thing.callForAction();
-        assertEquals("Food", value);
-    }
+  @Test
+  public void it_should_call_for_action() {
+    Thing thing = new Thing();
+    String value = thing.callForAction();
+    assertEquals("Food", value);
+  }
 
-    @Test
-    public void it_should_not_fail() {
-        assertTrue(true);
-    }
+  @Test
+  public void it_should_not_fail() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void setDirectonTest() {
+    Snake snake = new Snake();
+    snake.setDirection(1);
+
+    assertEquals(snake.getDirection(), 1);
+  }
 }
