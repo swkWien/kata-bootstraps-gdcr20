be6f90c57c3ab75168d79ac301605a98328e5d87
Samo Jelovsek <s.jelovsek@sportradar.com>
2021-12-07 12:00:07 +0100

    Add initial tests

--- golang/ginkgo/kata.go
+++ golang/ginkgo/kata.go
@@ -1,5 +1,33 @@
 package main
 
 func SayHello() string {
-	return "Fix this test!"
+	return "Hello"
+}
+
+const (
+	LEFT  = 0
+	RIGHT = 1
+	DOWN  = 2
+	UP    = 3
+)
+
+type SnakePart struct {
+	Direction int
+	Next      *SnakePart
+}
+
+func (s *SnakePart) Len() int {
+	if s == nil {
+		return 0
+	}
+
+	return s.Next.Len() + 1
+}
+
+func (s *SnakePart) IsHead(snake *SnakePart) bool {
+	if snake == nil {
+		return true
+	}
+
+	return *s == *snake
 }
--- golang/ginkgo/kata_test.go
+++ golang/ginkgo/kata_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -12,5 +13,48 @@ var _ = Describe("Something", func() {
 
 	It("passing", func() {
 		Expect(42).To(Equal(42))
+	})
+})
+
+var _ = Describe("SnakePart", func() {
+	It("get size", func() {
+		snake := SnakePart{Direction: LEFT, Next: nil}
+		snakeLen := snake.Len()
+
+		Expect(snakeLen).To(Equal(1))
+	})
+
+	It("get size", func() {
+		snake := SnakePart{Direction: LEFT, Next: &SnakePart{Direction: LEFT, Next: nil}}
+		snakeLen := snake.Len()
+
+		Expect(snakeLen).To(Equal(2))
+	})
+
+	It("is head?", func() {
+		snake := SnakePart{Direction: LEFT, Next: &SnakePart{Direction: LEFT, Next: nil}}
+
+		Expect(snake.IsHead(nil)).To(Equal(true))
+	})
+
+	It("is head?", func() {
+		tail := SnakePart{Direction: LEFT, Next: nil}
+		snake := SnakePart{Direction: LEFT, Next: &tail}
+
+		fmt.Println(snake)
+
+		Expect(tail.IsHead(&snake)).To(Equal(false))
+	})
+
+	It("are snakes equals?", func() {
+		tail := SnakePart{Direction: LEFT, Next: nil}
+		snake := SnakePart{Direction: LEFT, Next: &tail}
+
+		tail2 := SnakePart{Direction: LEFT, Next: nil}
+		snake2 := SnakePart{Direction: LEFT, Next: &tail2}
+
+		fmt.Println(snake)
+
+		Expect(snake2.IsHead(&snake)).To(Equal(true))
 	})
 })
