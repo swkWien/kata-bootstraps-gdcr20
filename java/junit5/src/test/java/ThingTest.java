4e8dcd16fa07beb4e2c1be614b6d032927667241
jfilipcic <j.filipcic@sportradar.com>
2022-07-04 07:41:12 +0200

    updated pom and added imports

--- java/junit5/src/test/java/ThingTest.java
+++ java/junit5/src/test/java/ThingTest.java
@@ -1,18 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.when;
 
 public class ThingTest {
 
+    @BeforeEach
+    void beforeEach() {
+        MockitoAnnotations.openMocks(this);
+    }
     @Test
     void fail() {
         Thing thing = new Thing();
         String value = thing.callForAction();
-        assertEquals("Food", value);
+        assertThat(value).isEqualTo("Food");
     }
 
     @Test
     void it_should_not_fail() {
-        assertTrue(true);
+        assertThat(true).isTrue();
+
     }
 }
