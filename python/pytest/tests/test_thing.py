fa1e9a96c135a0dbec1248efab8e6639a3857b09
Carlo Del Don <c.deldon@sportradar.com>
2022-07-05 10:39:43 +0200

    refactor product into enum

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -34,8 +34,11 @@ def test_should_throw_error_for_invalid_coin():
         coin_value(X())
 
 
-def product_prize(product):
-    if product == "cola":
-        return 100
+class Product(enum.Enum):
+    cola = 100
+
+def product_prize(product: Product):
+    return product.value
+
 def test_cola_product():
-    assert product_prize("cola") == 100
+    assert product_prize(Product.cola) == 100
