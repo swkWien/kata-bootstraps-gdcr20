6129476d61c84c66529b37ec131d53ddf73e9310
Stefan Boos <kontakt@boos.systems>
2022-07-05 15:39:12 +0200

    WIP

--- js/jest-tests/test/VendingMachine.test.js
+++ js/jest-tests/test/VendingMachine.test.js
@@ -1,7 +1,18 @@
 import HelloWorld from '../src/HelloWorld';
 
-function displayValue() {}
+function displayValue() {
+  return "INSERT COIN";
+}
 
 test('it should return INSERT COIN if no coin was inserted', () => {
   expect(displayValue()).toEqual("INSERT COIN");
+})
+
+function insertCoin(coin) {
+  return displayValue;
+}
+
+test('it should return "5" if Nickel was inserted', () => {
+  let theDisplayValue = insertCoin("Nickel")
+  expect(theDisplayValue()).toEqual("5");
 })
