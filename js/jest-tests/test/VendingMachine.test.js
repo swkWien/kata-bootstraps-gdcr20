6f41e2c5a34a60d427e653d39d42414218bd4f12
Ondrej Husar <o.husar@sportradar.com>
2022-07-05 15:56:39 +0200

    wip

--- js/jest-tests/test/VendingMachine.test.js
+++ js/jest-tests/test/VendingMachine.test.js
@@ -1,18 +1,42 @@
 import HelloWorld from '../src/HelloWorld';
 
+class Nickel {
+  get value() {
+    return 5;
+  }
+}
+
+class InsertCoin {
+  get value() {
+    return 'INSERT COIN';
+  }
+}
+
 function displayValue() {
-  return "INSERT COIN";
+  return new InsertCoin().value;
+}
+
+function displayValueOfState(state) {
+  return state.value.toString();
+}
+
+// Return type: { displayValue: Function, insertCoin: Function, ... }
+function insertCoin(coin) {
+  return () => displayValueOfState(coin);
 }
 
 test('it should return INSERT COIN if no coin was inserted', () => {
   expect(displayValue()).toEqual("INSERT COIN");
 })
 
-function insertCoin(coin) {
-  return displayValue;
-}
-
 test('it should return "5" if Nickel was inserted', () => {
-  let theDisplayValue = insertCoin("Nickel")
-  expect(theDisplayValue()).toEqual("5");
+  const { displayValue } = insertCoin(new Nickel());
+  expect(displayValue()).toEqual("5");
+})
+
+test('it should return "10" if 2 Nickels were inserted', () => {
+  const { insertCoin: insertCoinFn } = insertCoin(new Nickel());
+  const { displayValue } = insertCoinFn(new Nickel());
+
+  expect(displayValue()).toEqual("10");
 })
