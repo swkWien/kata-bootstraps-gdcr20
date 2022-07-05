56d798838feff87bd63a0a4f103eaf7b73b72a2b
Claus Aichinger <claus.aichinger@gmail.com>
2022-07-05 14:06:34 +0200

    Document requirements

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,3 +1,26 @@
+"""
+Session #1 / Kata
+Vending Machine
+see all details Vending Machine Kata
+
+products:
+* Cola - 1 $
+* Chips - 0,50 $
+* Candy - 0,65 $
+valid coins:
+* Penny - 1 cent
+* Nickel - 5 cents
+* Dime - 10 cents
+* Quarter - 25 cents
+
+Features
+* Display: "INSERT COIN", if one is inserted, show the value of the coin and update the current amount of money
+* Select product: if enough money has been inserted, present the product and display "THANK YOU" for 5 seconds
+* Make change: return the amount of money, that hasn't been used
+* Return coins button: return the placed coins and display "INSERT COIN"
+* Sold out: if product is out of stock, display "SOLD OUT" for 5 seconds
+* Exact change only: display "EXACT CHANGE ONLY" if the machine is not able to give chance.
+"""
 from app.thing import Thing
 
 
