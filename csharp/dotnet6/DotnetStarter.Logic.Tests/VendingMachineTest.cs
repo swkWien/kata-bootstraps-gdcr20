d64c5b24d835fdce916e432fce4af43262e243c5
Stefan Boos <kontakt@boos.systems>
2022-07-05 12:20:10 +0200

    test: allow testing displayed string

--- csharp/dotnet6/DotnetStarter.Logic.Tests/VendingMachineTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/VendingMachineTest.cs
@@ -7,21 +7,30 @@ namespace DotnetStarter.Logic.Tests
         [Fact]
         public void A_VendingMachine_IsCreated()
         {
-            var machine = CreateVendingMachine();
+            var machine = CreateVendingMachine(new DisplayMock());
             Assert.NotNull(machine);
+        }
+
+        [Fact]
+        public void A_VendingMachine_IsCreated_ShowsInsertCoin()
+        {
+            var displayMock = new DisplayMock();
+            CreateVendingMachine(displayMock);
+            Assert.Equal("", displayMock.LastMessage);
         }
 
         [Fact]
         public void A_VendingMachine_HasMoneyProperty()
         {
-            var machine = CreateVendingMachine();
+            var machine = CreateVendingMachine(new DisplayMock());
             Assert.Equal(0, machine.Money);
         }
 
-        public static VendingMachine CreateVendingMachine() => new(new DisplayMock());
+        public static VendingMachine CreateVendingMachine(DisplayMock displayMock) => new(displayMock);
     }
 
     public class DisplayMock : IDisplay
     {
+        public string LastMessage { get; set; } = "";
     }
 }
