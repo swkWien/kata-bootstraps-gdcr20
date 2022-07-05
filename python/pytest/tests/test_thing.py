d9dbf1d9c4f102b6d9a168b3014f88fc622a1816
Carlo Del Don <c.deldon@sportradar.com>
2022-07-05 10:26:52 +0200

    nickel-dime coin value working

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -4,6 +4,16 @@ import enum
 def coin_value(coin_identifier):
     if coin_identifier == "penny":
         return 1
+    if coin_identifier == "nickel":
+        return 5
+    if coin_identifier == "dime":
+        return 10
 
 def test_penny_coin_value():
     assert coin_value('penny') == 1
+
+def test_nickel_coin_value():
+    assert coin_value('nickel') == 5
+
+def test_dime_coin_value():
+    assert coin_value('dime') == 10
