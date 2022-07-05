732ffa0ad29d4a338f186295de28f42e5c83d8f3
Ondrej Husar <o.husar@sportradar.com>
2022-07-05 15:32:57 +0200

    WIP

---
+++ js/jest-tests/test/VendingMachine.test.js
@@ -0,0 +1,7 @@
+import HelloWorld from '../src/HelloWorld';
+
+function displayValue() {}
+
+test('it should return INSERT COIN if no coin was inserted', () => {
+  expect(displayValue()).toEqual("INSERT COIN");
+})
