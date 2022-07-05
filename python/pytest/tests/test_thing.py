9032eecc659665f0656b5cee5524bdc5ef5ed8ee
Carlo Del Don <c.deldon@sportradar.com>
2022-07-05 10:36:47 +0200

    raise for non-Coin

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,5 +1,7 @@
 import enum
 
+import pytest
+
 
 class Coin(enum.Enum):
     penny = 1
@@ -8,6 +10,8 @@ class Coin(enum.Enum):
     quarter = 25
 
 def coin_value(coin_identifier: Coin):
+    if not isinstance(coin_identifier, Coin):
+        raise ValueError("coin_identified should be an isntance of {}, but is {}".format(Coin, type(coin_identifier)))
     return coin_identifier.value
 
 def test_penny_coin_value():
@@ -22,3 +26,9 @@ def test_dime_coin_value():
 
 def test_quarter_coin_value():
     assert coin_value(Coin.quarter) == 25
+
+def test_should_throw_error_for_invalid_coin():
+    with pytest.raises(Exception):
+        class X:
+            value = 113
+        coin_value(X())
