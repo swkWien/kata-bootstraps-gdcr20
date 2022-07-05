a1031435ea3b2c5ef997d4bfd9326315cdbb9234
Claus Aichinger <claus.aichinger@gmail.com>
2022-07-05 14:05:17 +0200

    Init green setup

--- python/pytest/tests/test_thing.py
+++ python/pytest/tests/test_thing.py
@@ -1,11 +1,6 @@
 from app.thing import Thing
 
 
-def test_correct_greeting():
-    thing = Thing("Bob")
-    assert "Hello Bob!" == thing.return_hello_name()
-
-
 def test_fail():
     thing = Thing("Albert")
-    assert "Wrong!" == thing.return_hello_name()
+    assert "Hello Albert!" == thing.return_hello_name()
