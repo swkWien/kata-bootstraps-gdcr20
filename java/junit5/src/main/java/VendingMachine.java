492e971dae55d3c050899f98c7707f66740e6d76
David Seebacher <david.seebacher@resourex.com>
2022-11-29 14:40:40 +0100

    VM should only specified coins

--- java/junit5/src/main/java/VendingMachine.java
+++ java/junit5/src/main/java/VendingMachine.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class VendingMachine {
 	
 	int totalValue = 0;
 	
 	public int insertCoin(int i) {
-		if (i >= 5) {
+		if (Arrays.stream(Coin.values()).anyMatch(coin ->coin.getValue() == i)) {
 			totalValue += i;
 		}
 		return totalValue;
