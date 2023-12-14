83a867ddfb017ac7e848a232a86f85db4aaba53f
s.mahdavi <s.mahdavi@sportradar.com>
2023-12-14 10:50:59 +0100

    Added getSize test method

--- ts/typescript-jest/src/MapGenerator.test.ts
+++ ts/typescript-jest/src/MapGenerator.test.ts
@@ -1,11 +1,20 @@
 class OurMap {
 
+    getSize(): ISize {
+        return {width: 5, height: 5}
+    }
+
 }
 
 class MapGenerator {
     createMap(): OurMap {
         return new OurMap();
-    }
+    }    
+}
+
+interface ISize {
+    width: number;
+    height: number;
 }
 
 describe('MapGenerator', () => {
@@ -18,5 +27,12 @@ describe('MapGenerator', () => {
         expect(map).toBeInstanceOf(OurMap)
     });
 
+    it('should create default Map [5,5]', () => {
+        const mapGenerator = new MapGenerator();
+
+        const map = mapGenerator.createMap();
+
+        expect(map.getSize()).toEqual({width: 5, height: 5});
+    });
 
 });
