c3d5c508c412b6fa27ddce69ce10be943ded5819
Gerald Vrana <gerald.vrana@superluminar.io>
2023-12-14 10:12:47 +0100

    RED: creates pacman

---
+++ ts/typescript-jest/src/Pacman.spec.ts
@@ -0,0 +1,10 @@
+describe('Pacman', () => {
+
+    it('should have 3 lives on start', () => {
+        const expectedLives = 3;
+
+        const pacman = new Pacman();
+
+        expect(pacman.getLives()).toBe(expectedLives);
+    });
+});
